a895a7f55a0a107e870412099838d77c1c2cf6f3
Jos Bakker <josbakkeramsterdam@gmail.com>
2023-10-30 11:55:05 +0100

    new background no buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="background">
+        <h1>Welcome Poker Players of Middle Earth!</h1>
+        <h2>Log in to find your game of choice, or to create one.</h2>
+        <h2>New to this site? Then please register here.</h2>
       </div>
-      <h1>Jos is working it! what a BALLER</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
+
 
 export default App
