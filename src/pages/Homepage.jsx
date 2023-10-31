76f8586959d1e285d0aec47fe736b6ceb37ce733
Jos Bakker <josbakkeramsterdam@gmail.com>
2023-10-31 16:45:30 +0100

    the background is here, not all links working

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -23,24 +23,3 @@ function Homepage() {
 }
 
 export default Homepage;
-
-
-// function Homepage() {
-//     return (
-
-
-//         <div className="app">
-
-//             <div className="background">
-//                 <h1>Welcome Poker Players of Middle Earth!</h1>
-//                 <h2>Log in to find your game of choice, or to create one.</h2>
-//                 <h2>New to this site? Then please register here.</h2>
-
-
-//             </div>
-//         </div>
-//     );
-// }
-
-
-// export default Homepage
