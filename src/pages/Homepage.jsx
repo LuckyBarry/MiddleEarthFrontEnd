7ed44c649760977812db94143b6c11ac167a209e
Jos Bakker <josbakkeramsterdam@gmail.com>
2023-10-31 23:09:43 +0100

    navbar up top plus little css part two

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,19 +5,18 @@ import { Link } from 'react-router-dom';
 function Homepage() {
     return (
         <div className="home-page">
-            <div className="text-background">
 
                 <div className="welcomeBackground">
-                    <h1>Welcome Poker Players of Middle Earth!</h1>
+                <h1>Welcome Poker Players!</h1>
+                <h1>to the Middle Earth rounders club</h1>
                 </div>
 
                 <div className="loginHomePage">
-                    <Link to="/LoginPage">Log in to find your game of choice, or create your own!.</Link>
+                <Link to="/LoginPage">Allready a regular? LOG IN.</Link>
                 </div>
 
                 <h2><br></br>
-                    <Link to="/SignUpPage">New to this site? Please register here.</Link></h2>
-            </div>
+                <Link to="/SignUpPage">New to this site? Please register here.</Link></h2>
         </div >
     );
 }
